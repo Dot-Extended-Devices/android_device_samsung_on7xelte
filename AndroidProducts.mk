9b1fe694ad21df0d12704d9ee698c07a7793b9eb
bet4sh4dow <52181172+bet4sh4dow@users.noreply.github.com>
2019-10-30 21:47:15 -0300

    on7xelte

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -22,5 +22,5 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/bootleg_on7xelte.mk \
     $(LOCAL_DIR)/legion_on7xelte.mk \
     $(LOCAL_DIR)/lean_on7xelte.mk \
-    $(LOCAL_DIR)/colt_on7xelte.mk \
+    $(LOCAL_DIR)/aokp_on7xelte.mk \
     $(LOCAL_DIR)/aosip_on7xelte.mk \
